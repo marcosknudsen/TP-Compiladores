ed32fcc2927f9263e56938dc518954c0a76a22ec
Marcos Knudsen <marcosknu14@gmail.com>
2025-09-25 19:16:37 -0300

    number range exception

--- src/main/java/ar/tp/acciones/FinishConstant.java
+++ src/main/java/ar/tp/acciones/FinishConstant.java
@@ -9,26 +9,33 @@ import ar.tp.lexer.Lex;
 
 public class FinishConstant extends SemanticAction {
 
-        @Override
-        public Pointer ejecutar(BufferedReader sourceCode, Lex lex, int currentChar,
-                        HashMap<String, Symbol> symbols, HashMap<String, Integer> reservedWords) throws IOException {
-                try {
-                        Symbol symbol = symbols.get(lex.getString());
-                        if (symbol == null)
-                                symbols.put(lex.getString(),
-                                                new Symbol(Integer.parseInt(lex.getString()) > 65535 ? "longint"
-                                                                : "uinteger", "Constante"));
-                        lex.yylval = lex.getString();
-                        sourceCode.reset();
-                        if (Integer.parseInt(lex.getString()) > 65535)
-                                return new Pointer(273);
-                        else
-                                return new Pointer(272);
+    @Override
+    public Pointer ejecutar(BufferedReader sourceCode, Lex lex, int currentChar,
+                            HashMap<String, Symbol> symbols, HashMap<String, Integer> reservedWords) throws IOException {
+        try {
+            Symbol symbol = symbols.get(lex.getString());
+            if (symbol == null) {
+                if (Integer.parseInt(lex.getString()) < -32768 || Double.parseDouble(lex.getString()) > Double.parseDouble( "4294967295"))
+                    throw new NumberFormatException();
 
-                } catch (NumberFormatException e) {
-                        System.out.println("ERROR on line " + lex.line
-                                        + ": El valor ingresado no se encuentra dentro del rango aceptado");
-                        return new Pointer(-1);
-                }
+                symbols.put(
+                        lex.getString(),
+                        new Symbol(
+                                Integer.parseInt(lex.getString()) > 65535 ? "longint"
+                                        : "uinteger",
+                                "Constante"));
+            }
+            lex.yylval = lex.getString();
+            sourceCode.reset();
+            if (Integer.parseInt(lex.getString()) > 65535)
+                return new Pointer(273);
+            else
+                return new Pointer(272);
+
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR on line " + lex.line
+                    + ": El valor ingresado no se encuentra dentro del rango aceptado");
+            return new Pointer(-1);
         }
+    }
 }
--- src/main/java/ar/tp/acciones/FinishConstantNewLine.java
+++ src/main/java/ar/tp/acciones/FinishConstantNewLine.java
@@ -9,36 +9,40 @@ import ar.tp.lexer.Lex;
 
 public class FinishConstantNewLine extends SemanticAction {
 
-        @Override
-        public Pointer ejecutar(
-                        BufferedReader sourceCode,
-                        Lex lex,
-                        int currectChar,
-                        HashMap<String, Symbol> symbols,
-                        HashMap<String, Integer> reservedWords) throws IOException {
-                lex.line++;
-                try {
-                        Symbol symbol = symbols.get(lex.getString());
-                        if (symbol == null)
-                                symbols.put(
-                                                lex.getString(),
-                                                new Symbol(
-                                                                Integer.parseInt(lex.getString()) > 65535 ? "longint"
-                                                                                : "uinteger",
-                                                                "Constante"));
-                        sourceCode.reset();
-                        lex.yylval = lex.getString();
-                        if (Integer.parseInt(lex.getString()) > 65535)
-                                return new Pointer(
-                                                273);
-                        else
-                                return new Pointer(272);
-                } catch (NumberFormatException e) {
-                        System.out.println(
-                                        "ERROR on line " +
-                                                        lex.line +
-                                                        ": El valor ingresado no se encuentra dentro del rango aceptado");
-                        return new Pointer(-1);
-                }
+    @Override
+    public Pointer ejecutar(
+            BufferedReader sourceCode,
+            Lex lex,
+            int currectChar,
+            HashMap<String, Symbol> symbols,
+            HashMap<String, Integer> reservedWords) throws IOException {
+        lex.line++;
+        try {
+            Symbol symbol = symbols.get(lex.getString());
+            if (symbol == null) {
+                if (Integer.parseInt(lex.getString()) < -32768 || Double.parseDouble(lex.getString()) > Double.parseDouble( "4294967295"))
+                    throw new NumberFormatException();
+
+                symbols.put(
+                        lex.getString(),
+                        new Symbol(
+                                Integer.parseInt(lex.getString()) > 65535 ? "longint"
+                                        : "uinteger",
+                                "Constante"));
+            }
+            sourceCode.reset();
+            lex.yylval = lex.getString();
+            if (Integer.parseInt(lex.getString()) > 65535)
+                return new Pointer(
+                        273);
+            else
+                return new Pointer(272);
+        } catch (NumberFormatException e) {
+            System.out.println(
+                    "ERROR on line " +
+                            lex.line +
+                            ": El valor ingresado no se encuentra dentro del rango aceptado");
+            return new Pointer(-1);
         }
+    }
 }
