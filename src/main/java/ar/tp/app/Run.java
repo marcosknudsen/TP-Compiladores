fbb247e053aabb202ed99ff5542b181d42ec6ac9
Marcos Knudsen <marcosknu14@gmail.com>
2025-11-28 19:45:46 -0300

    output folder

--- src/main/java/ar/tp/app/Run.java
+++ src/main/java/ar/tp/app/Run.java
@@ -4,6 +4,9 @@ import ar.tp.generacion.GeneradorAsm;
 import ar.tp.lexer.Lex;
 import ar.tp.parser.*;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 import static ar.tp.parser.Parser.mostrarPila;
 import static ar.tp.parser.Parser.mostrarReglas;
 
@@ -26,10 +29,12 @@ public class Run {
         mostrarPila(Parser.pila);
         System.out.println(rc == 0 ? "Parse OK" : "Parse con errores");
 
+        String filename = new File(args[0]).getName().split("\\.")[0];
+
         if (rc == 0) {
             GeneradorAsm gen = new GeneradorAsm(Parser.reglas, lexer.symbols);
-            gen.generar("out.asm");
-            System.out.println("Assembler generado en out.asm");
+            gen.generar("out/" + filename + ".asm");
+            System.out.println("Assembler generado en out/" + filename + ".asm");
         }
     }
 }
